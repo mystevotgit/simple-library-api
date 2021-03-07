ea84ccd683ca77ccc0972c66767f3b4853d1892b
mystevotgit <oyeolatunji@gmail.com>
2021-03-07 22:58:53 +0100

    create logindto

---
+++ src/main/java/com/accessment/library/dto/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.accessment.library.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class LoginDTO {
+    private String emailId;
+    private String password;
+}
